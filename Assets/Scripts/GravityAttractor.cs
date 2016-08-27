c40c2411e526125d703218dd2af3b148cb0676d2
Håkon Solheim Marcus <hakonmarcus@gmail.com>
2016-08-27 13:21:58 +0200

    Fra ingenting til basic simulasjon av gravitasjon omking en planet, og en karakter som kan bevege seg rundt planeten og fly litt.

---
+++ Assets/Scripts/GravityAttractor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityAttractor : MonoBehaviour {
+
+	public float gravity;
+
+	public void Attract(Transform body)
+	{
+		Vector2 gravityUp = (body.position - transform.position).normalized;
+		Vector2 bodyUp = body.up;
+
+		Rigidbody2D rb2d;
+		rb2d = body.GetComponent<Rigidbody2D>();
+
+		rb2d.AddForce (gravityUp * gravity);
+
+		Quaternion targetRotation = Quaternion.FromToRotation (bodyUp, gravityUp) * body.rotation;
+		body.rotation = Quaternion.Slerp (body.rotation, targetRotation, 50 * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/GravityBody.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityBody : MonoBehaviour
+{
+	public GravityAttractor attractor;
+	private Transform thisTransform; 
+
+	// Use this for initialization
+	void Start ()
+	{
+		thisTransform = transform;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		attractor.Attract (thisTransform);
+	}
+}
